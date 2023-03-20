863d53a13da75552ac5992f774336d64c1df16cb
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2023-03-20 20:53:53 +0100

    Provide JVMTI agent and JNI library points

--- src/c/ubench.c
+++ src/c/ubench.c
@@ -19,33 +19,148 @@
 #include "ubench.h"
 
 #pragma warning(push, 0)
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 #include <jni.h>
 #include <jvmti.h>
 #pragma warning(pop)
 
-JNIEXPORT jint JNICALL
-Agent_OnLoad(
-	JavaVM* jvm, char* UNUSED_PARAMETER(options), void* UNUSED_PARAMETER(reserved)
+//
+
+static void JNICALL
+jvmti_callback_on_vm_init(
+	jvmtiEnv* UNUSED_PARAMETER(jvmti), JNIEnv* UNUSED_PARAMETER(jni),
+	jthread UNUSED_PARAMETER(thread)
 ) {
+	//
+	// Set the static boolean field 'alreadyLoaded' in the 'UbenchAgent' class to 'true'
+	// to indicate that native methods are already bound, because the library has been
+	// already loaded through the agent mechanism. This will prevent 'UbenchAgent' from
+	// trying to load the library explicitly. Even though the library can handle it, the
+	// 'loadLibrary()' call could fail if the 'java.library.path' is not set correctly.
+	//
+	jclass loader_class = (*jni)->FindClass(jni, "cz/cuni/mff/d3s/perf/UbenchAgent");
+	if (loader_class == NULL) {
+		fprintf(stderr, "warning: could not find 'UbenchAgent' class\n");
+		return;
+	}
+
+	jfieldID field_id = (*jni)->GetStaticFieldID(jni, loader_class, "alreadyLoaded", "Z");
+	if (field_id == NULL) {
+		fprintf(stderr, "warning: could not find 'alreadyLoaded' field in the 'UbenchAgent' class\n");
+		return;
+	}
+
+	(*jni)->SetStaticBooleanField(jni, loader_class, field_id, JNI_TRUE);
+}
+
+static jvmti_context_t init_context = {
+	.callbacks = { .VMInit = &jvmti_callback_on_vm_init },
+	.events = { JVMTI_EVENT_VM_INIT, 0 }
+};
+
+static bool
+ubench_signal_agent_loaded_on_vm_init(JavaVM* jvm) {
+	assert(jvm != NULL);
+	return ubench_jvmti_context_init_and_enable(&init_context, jvm);
+}
+
+//
+
+static bool
+ubench_startup(JavaVM* jvm) {
+	assert(jvm != NULL);
+
+	// Avoid initializing the library twice on startup.
+	static ubench_atomic_int_t startup_guard = { .atomic_value = 0 };
+
+	bool initialized = ubench_atomic_int_inc(&startup_guard) != 0;
+	if (initialized) {
+		DEBUG_PRINTF("ubench already initialized.");
+		return true;
+	}
+
+	//
+	// Initialize the individual modules. Some of them are allowed to fail,
+	// because they will try to use JVMTI which may be unavailable (e.g., in
+	// the native image). This will make some functionality unavailable, but
+	// will still allow using the agent e.g., to use PAPI.
+	//
+	DEBUG_PRINTF("initializing counters module");
 	if (!ubench_counters_init(jvm)) {
-		return JNI_ERR;
+		fprintf(stderr, "warning: failed to initialize event counter module.\n");
 	}
 
+	DEBUG_PRINTF("initializing threads module");
 	if (!ubench_threads_init(jvm)) {
-		return JNI_ERR;
+		fprintf(stderr, "warning: failed to initialize threads module.\n");
 	}
 
+	DEBUG_PRINTF("initializing benchmark module");
 	if (!ubench_benchmark_init()) {
-		return JNI_ERR;
+		fprintf(stderr, "error: failed to initialize benchmark module.\n");
+		return false;
 	}
 
-	DEBUG_PRINTF("initialization completed successfully.");
-	return JNI_OK;
+	// When the VM is initialized, signal that the agent has been loaded.
+	// This will not work in an environment without JVMTI (e.g., native image),
+	// which means that the library must have been loaded explicitly.
+	if (!ubench_signal_agent_loaded_on_vm_init(jvm)) {
+		DEBUG_PRINTF("unable to signal that the agent has been loaded.");
+	}
+
+	return true;
+}
+
+static void
+ubench_shutdown(JavaVM* UNUSED_PARAMETER(jvm)) {
+	assert(jvm != NULL);
+
+	// Avoid finalizing the library twice on shutdown.
+	static ubench_atomic_int_t shutdown_guard = { .atomic_value = 0 };
+
+	bool finalized = ubench_atomic_int_inc(&shutdown_guard) != 0;
+	if (finalized) {
+		DEBUG_PRINTF("ubench already finalized.");
+		return;
+	}
+
+	// So far, there is nothing to do on shutdown.
+	// We do not dispose the JVMTI environments.
+}
+
+//
+
+JNIEXPORT jint JNICALL
+Agent_OnLoad(JavaVM* vm, char* UNUSED_PARAMETER(options), void* UNUSED_PARAMETER(reserved)) {
+	DEBUG_PRINTF("agent loading started.");
+	bool success = ubench_startup(vm);
+	DEBUG_PRINTF("agent loading finished (%s).", success ? "success" : "failure");
+	return success ? JNI_OK : JNI_ERR;
 }
 
 JNIEXPORT void JNICALL
-Agent_OnUnload(JavaVM* UNUSED_PARAMETER(jvm)) {
-	DEBUG_PRINTF("agent unloaded.");
+Agent_OnUnload(JavaVM* vm) {
+	DEBUG_PRINTF("agent unloading started.");
+	ubench_shutdown(vm);
+	DEBUG_PRINTF("agent unloading finished.");
+}
+
+//
+
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM* jvm, void* UNUSED_PARAMETER(reserved)) {
+	DEBUG_PRINTF("library loading started.");
+	bool success = ubench_startup(jvm);
+	DEBUG_PRINTF("library loading finished (%s).", success ? "success" : "failure");
+	return success ? JNI_VERSION_1_8 : 0;
+}
+
+JNIEXPORT void JNICALL
+JNI_OnUnload(JavaVM* jvm, void* UNUSED_PARAMETER(reserved)) {
+	DEBUG_PRINTF("library unloading started.");
+	ubench_shutdown(jvm);
+	DEBUG_PRINTF("library unloading finished.");
 }
