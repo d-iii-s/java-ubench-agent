472bae3283f1153236f20b1894fee9cc22b92ca5
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2023-04-11 15:24:38 +0200

    Fix copyright notice in jvmutil

--- src/c/jvmutil.c
+++ src/c/jvmutil.c
@@ -1,6 +1,5 @@
 /*
- * Copyright 2014 Charles University in Prague
- * Copyright 2014 Vojtech Horky
+ * Copyright 2023 Charles University in Prague
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
